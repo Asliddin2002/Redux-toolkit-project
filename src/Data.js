09af86fa09d3f270bc52e2da36c6a1be9cb39cf5
Asliddin2002 <wiut00012247@gmail.com>
2022-05-05 01:49:07 +0500

    first commit

---
+++ src/Data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {id:1,
+        name:"iphone 13 Pro",
+        price:8,
+        desc:"Modern mobile phone with big memory and multifuntion"
+    },
+    {id:2,
+        name:"Samsung 21 Ultra",
+        price:7,
+        desc:"Modern mobile phone with good camera and multifuntion"
+    },
+    {id:3,
+        name:"Oppo",
+        price:6,
+        desc:"Modern gaming mobile phone with big memory and multifuntion"
+    }
+]
